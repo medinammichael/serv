aa008cf70ad6f9c3af7e090318d7a208a97e22b3
medinammichael <medina.mmichael@gmail.com>
2026-04-14 02:54:07 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# caos_scripts
---
+++ config.py
@@ -0,0 +1,9 @@
+# place any of your own overrides here.
+# see bombsquad_server for details on what you can override
+# examples (uncomment to use):
+#config['partyName'] = u'\U0001F3C6TRONOS SMASH TEAMS\U0001F3C6 By MICHAEL'
+#config['sessionType'] = 'ffa'
+#config['maxPartySize'] = 15
+#config['port'] = 43211
+#config['playlistCode'] = 528507
+#config['statsURL'] = ''
